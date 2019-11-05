b3b8514aa139d1244705ec07a15fdf1e94fabdce
willjoe1987 <littlefish_0130@163.com>
2019-11-06 02:09:32 +0800

    chinaapp brand

--- tx_samples/src/main/java/org/tx_samples/App.java
+++ tx_samples/src/main/java/org/tx_samples/App.java
@@ -1,13 +1,80 @@
 package org.tx_samples;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	
+	//MAIGOO
+	static String cataUrl = "https://www.maigoo.com/ajaxstream/category/?action=searchblowcategory&catid=7&block=brand";
+	//www.chinapp.com
+	static String pinpai = "https://www.chinapp.com/pinpai/canyin-0-0-";
+	static String catagory = "餐饮行业";
+	
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+    	RequestConfig requestConfig = 
+                RequestConfig.custom().setConnectTimeout(5000).setConnectionRequestTimeout(1000).build();  
+    	
+    	CloseableHttpClient  hc = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
+    	CloseableHttpResponse res = null;
+    	File target = new File("D:/chinaapp_pinpai_"+catagory+".sql");
+		FileOutputStream output = new FileOutputStream(target);
+		OutputStreamWriter write = new OutputStreamWriter(output,"UTF-8");
+    	if(!target.exists()) {
+			target.createNewFile();
+		}
+    	int index = 1;
+    	
+    	for(int i=1;i<159;i++) {
+    		
+    		HttpGet hg = new HttpGet(pinpai+i);
+    		res = hc.execute(hg);
+    		HttpEntity he = res.getEntity();
+    		
+    		String e =  EntityUtils.toString(he) ;
+    		Document doc = Jsoup.parse(e);
+    		//"//li[@class=pplb_item]
+    		Elements as = doc.getElementsByClass("pplb_item");
+    		Iterator<Element> ai = as.iterator();
+    		while(ai.hasNext()) {
+    			Element li = ai.next();
+    			Element a = li.child(0);
+    			String href = a.attr("href");
+    			String name = a.attr("title");
+    			Element logoimg = a.child(0);
+    			String logourl = logoimg.attr("src");
+    			String sql = "Insert into chinaapp_pinpai (name, href, logourl, gatagory) values "
+    					+ "('"+name+"','"+href+"','"+logourl+"','"+catagory+"');\n";
+    			
+    			System.out.print("【"+index+"】："+sql);
+    			index ++ ;
+    			write.write(sql);
+    		}
+    		Thread.sleep(Math.round(Math.random()*5000));
+    	}
+    	write.flush();
+		write.close();
+        //System.out.println(e);
     }
+    
 }
--- webmagic-samples/src/main/java/us/codecraft/webmagic/model/samples/QQMeishi.java
+++ webmagic-samples/src/main/java/us/codecraft/webmagic/model/samples/QQMeishi.java
@@ -9,18 +9,18 @@ import us.codecraft.webmagic.model.annotation.TargetUrl;
 /**
  * @author code4crafter@gmail.com
  */
-@TargetUrl("http://meishi.qq.com/beijing/c/all[\\-p2]*")
-@ExtractBy(value = "//ul[@id=\"promos_list2\"]/li",multi = true)
+@TargetUrl("https://www.chinapp.com/pinpai/canyin-0-0-[1-9]*")
+//@ExtractBy(value = "//ul[@class=pplb_items_lists]/")
 public class QQMeishi {
 
-    @ExtractBy("//div[@class=info]/a[@class=title]/h4/text()")
+    @ExtractBy("//li[@class=pplb_item]//a/@title")
     private String shopName;
 
-    @ExtractBy("//div[@class=info]/a[@class=title]/text()")
-    private String promo;
+//    @ExtractBy("//div[@class=info]/a[@class=title]/text()")
+//    private String promo;
 
     public static void main(String[] args) {
-        OOSpider.create(Site.me(), new ConsolePageModelPipeline(), QQMeishi.class).addUrl("http://meishi.qq.com/beijing/c/all").thread(4).run();
+        OOSpider.create(Site.me(), new ConsolePageModelPipeline(), QQMeishi.class).addUrl("https://www.chinapp.com/pinpai/canyin-0-0-1").thread(4).run();
     }
 
 }
